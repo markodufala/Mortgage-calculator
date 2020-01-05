3d7c8e4cedfb7a312a482d0615b1e72cd581bde0
Marko <50919055+nameMark@users.noreply.github.com>
2020-01-05 18:07:03 +0100

    Update Main.java

--- code/Main.java
+++ code/Main.java
@@ -70,45 +70,6 @@ public class Main {
 
 
 
-
-package com.company;
-
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String unitSystemInput;
-
-        
-
-
-        //System.out.println(unitSystemInput);
-
-
-        while (true){
-            System.out.print("From wich unit system you want to convert?:");
-            // First String input
-            unitSystemInput = scanner.nextLine().toLowerCase().trim();
-            if (unitSystemInput.equals("metric"))
-                System.out.println("Do you want to convert to metric values or imperial unit system ?");
-            else if (unitSystemInput.equals("imperial"))
-                System.out.println("Do you want to convert to imperial values or metric unit system ?");
-            else if (!unitSystemInput.equals("metric") && !unitSystemInput.equals("imperial"))
-                break;
-        }
-        System.out.println("Invalid input! Enter value again:");
-    }
-}
-
-
-
-
-
-
-
-
 package com.company;
 
 import java.util.Scanner;
@@ -122,11 +83,10 @@ public class Main {
         boolean firstStringAnswer;
 
 
-
         //System.out.println(unitSystemInput);
 
 
-        while (true){
+        while (true) {
             System.out.print("From wich unit system you want to convert?:");
             // First String input
             unitSystemInput = scanner.nextLine().toLowerCase().trim();
@@ -137,36 +97,35 @@ public class Main {
             }
             System.out.println("Invalid input! Enter value again. Write Metric or Imperial.:");
         }
-        while (true){
-            if (unitSystemInput.equals("metric")){
-                System.out.println("Do you want to convert to metric values or imperial unit system ?");
+
+        //if (unitSystemInput.equals("metric"))
+        //  System.out.print("Do you want to convert to metric values or imperial unit system?:");
+        //else if (unitSystemInput.equals("imperial"))
+        //System.out.print("Do you want to convert to imperial values or metric unit system?:");
+
+
+        //System.out.print("Do you want to convert to metric values or imperial unit system?:");
+        //convertingBetween = scanner.nextLine().toLowerCase().trim();
+
+        while (true) {
+            if (unitSystemInput.equals("metric")) {
+                System.out.print("Do you want to convert to metric values or imperial unit system?:");
                 convertingBetween = scanner.nextLine().toLowerCase().trim();
-                if ((convertingBetween.equals("metricvalues")) || (convertingBetween.equals("imperialunitsystem")));{
-                    System.out.println(convertingBetween);
+                if (convertingBetween.equals("metric values")) {
+                    System.out.println("write down your values");
                     break;
                 }
             }else if (unitSystemInput.equals("imperial")){
-                System.out.println("Do you want to convert to imperial values or metric unit system ?");
-                convertingBetween = scanner.nextLine().toLowerCase().trim();
-                System.out.println(convertingBetween);
-                break;
-            }
-            System.out.println("Invalid input! Enter value again:");
-        }
-    }
-}
-
-
-
-
-
-
-
-        while (true) {
-            if ((unitSystemInput.equals("imperial"))) {
                 System.out.print("Do you want to convert to imperial values or metric unit system?:");
                 convertingBetween = scanner.nextLine().toLowerCase().trim();
-                System.out.println(convertingBetween);
-                break;
-            }
+                if (convertingBetween.equals("imperial values")) {
+                    System.out.println("write down your values second part of code");
+                    break;
+                }
+
+            }System.out.println("Invalid input! Enter value again.:");
         }
+
+
+    }
+}
